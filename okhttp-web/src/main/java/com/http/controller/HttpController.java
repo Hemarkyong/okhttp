54e7ceb3f893cdf519cc7a66935ebb2b2360f436
Hemarkyong <76929894@qq.com>
2022-10-06 13:21:48 +0800

    1

--- okhttp-web/src/main/java/com/http/controller/HttpController.java
+++ okhttp-web/src/main/java/com/http/controller/HttpController.java
@@ -1,12 +1,10 @@
 package com.http.controller;
 
 import com.http.ResutlResponse;
-import com.http.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HttpController {
